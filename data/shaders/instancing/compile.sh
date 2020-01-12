ed2b52f979fd26e645ab51a62b52c327e851dee3
David <david@knight.fm>
2020-01-11 20:15:39 -0800

    shader compile script

---
+++ data/shaders/instancing/compile.sh
@@ -0,0 +1,4 @@
+${VULKAN_SDK}/bin/glslc instancing.vert -o instancing.vert.spv
+${VULKAN_SDK}/bin/glslc instancing.frag -o instancing.frag.spv
+
+${VULKAN_SDK}/bin/glslc starfield.frag -o starfield.frag.spv
